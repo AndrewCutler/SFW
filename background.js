cb48af6894eb3a2c9aa0c40669222b3f3ff29d11
Andrew <spyroninja@gmail.com>
2019-05-26 15:38:31 -0400

    keyboard shortcut Ctrl+M to open SFW windows

--- background.js
+++ background.js
@@ -1,8 +1,18 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
-  chrome.windows.create({
-    "url": ['http://www.google.com', 'http://www.mlb.com'],
-    "state": 'maximized',
-    "focused": true
-  })
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//   chrome.windows.create({
+//     url: ['http://www.google.com', 'http://www.mlb.com'],
+//     state: 'maximized',
+//     focused: true
+//   })
+//   console.log('popup')
+// })
 
-});
+chrome.commands.onCommand.addListener(function(command) {
+  if (command === 'open-sfw') {
+    chrome.windows.create({
+      url: ['http://www.google.com', 'http://www.mlb.com'],
+      state: 'maximized',
+      focused: true
+    })
+  }
+})
