047bc1b01f79248cc1478f6b85da5fdebb18d793
Andrew Cutler <Andrew.Cutler@csi.cuny.edu>
2019-05-28 14:15:53 -0400

    retrieving saved URLs from storage

--- popup.js
+++ popup.js
@@ -1,10 +1,10 @@
-var urls = []
 var url = document.getElementById("url")
+var site = document.getElementById("site")
 
 //add URL
 var addBtn = document.getElementById("add")
 addBtn.onclick = function () {
-  chrome.storage.local.set({ 'url': url.value }, function () {
+  chrome.storage.local.set({ [site.value]: url.value }, function () {
     console.log('URL ' + url.value + ' saved.')
   })
   return false
@@ -13,11 +13,15 @@ addBtn.onclick = function () {
 //retrieve URLs
 var showBtn = document.getElementById('show')
 showBtn.onclick = function () {
-  chrome.storage.local.get(['url'], function (result) {
+  chrome.storage.local.get(null, function (result) {
+    console.log(result)
     var list = document.getElementById('list')
-    var node = document.createElement('LI')
-    var text = document.createTextNode(result.url)
-    node.appendChild(text)
-    list.appendChild(node)
+    var urls = Object.keys(result)
+    urls.forEach(s => {
+      var node = document.createElement('LI')
+      var text = document.createTextNode(s)
+      node.appendChild(text)
+      list.appendChild(node)
+    })
   })
 }
