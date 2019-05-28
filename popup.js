f850be5b61b09b0da5ca3443816a754ae8c883ea
Andrew Cutler <Andrew.Cutler@csi.cuny.edu>
2019-05-28 12:06:10 -0400

    added popup, started using storage

--- popup.js
+++ popup.js
@@ -1,4 +1,23 @@
-var sfwButton = document.getElementById('safe')
-sfwButton.onclick = function (el) {
-  chrome.windows.create({ url: ['http://www.google.com', 'http://www.mlb.com'], state: 'maximized', focused: true })
+var urls = []
+var url = document.getElementById("url")
+
+//add URL
+var addBtn = document.getElementById("add")
+addBtn.onclick = function () {
+  chrome.storage.local.set({ 'url': url.value }, function () {
+    console.log('URL ' + url.value + ' saved.')
+  })
+  return false
+}
+
+//retrieve URLs
+var showBtn = document.getElementById('show')
+showBtn.onclick = function () {
+  chrome.storage.local.get(['url'], function (result) {
+    var list = document.getElementById('list')
+    var node = document.createElement('LI')
+    var text = document.createTextNode(result.url)
+    node.appendChild(text)
+    list.appendChild(node)
+  })
 }
