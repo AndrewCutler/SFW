382c9f7eaae51427ac0ac9914c0d6b0413d63ed0
Andrew Cutler <andrewkcutler@gmail.com>
2019-05-30 13:20:30 -0400

    added noneDiv logic for empty lists

--- popup/popup.js
+++ popup/popup.js
@@ -2,7 +2,7 @@
 var url = document.getElementById("url")
 var site = document.getElementById("site")
 //get list ul from DOM
-var list = document.getElementById('list')
+var list = document.getElementById("list")
 //toggle for list
 var listShown = false
 //regex for https:// and http://
@@ -10,22 +10,41 @@ var urlRE = /^https?:\/\//i
 
 //add URL
 var addBtn = document.getElementById("add")
-addBtn.onclick = function () {
+addBtn.onclick = function() {
   //first get previous urls
   showURLs()
   //then add new one
-  chrome.storage.sync.set({ [site.value]: validateURL(url.value) }, function () {
+  chrome.storage.sync.set({ [site.value]: validateURL(url.value) }, function() {
     addSiteToList(site.value, url.value)
   })
 }
 
 //retrieve URLs
-var showBtn = document.getElementById('show')
-showBtn.onclick = function () {
+var showBtn = document.getElementById("show")
+showBtn.onclick = function() {
   //show list of URLs
   if (!listShown) {
-    showURLs()
-  }
+    //get sites
+    chrome.storage.sync.get(null, function(result) {
+      var showSection = document.getElementById("showSection")
+      //if no sites and noneDiv not yet there, add it
+      if (
+        !(Object.keys(result).length > 0) &&
+        showSection.lastElementChild.tagName !== "DIV"
+      ) {
+        var noneText = document.createTextNode(
+          "No sites configured. Add sites you wish to open."
+        )
+        var noneDiv = document.createElement("div")
+        noneDiv.setAttribute("id", "noneDiv")
+        noneDiv.appendChild(noneText)
+        showSection.appendChild(noneDiv)
+      } else {
+        //otherwise show sites saved
+        showURLs()
+      } //end else
+    }) //end if empty
+  } //end if listShown = false
   //otherwise, collapse list
   else {
     while (list.hasChildNodes()) list.removeChild(list.lastChild)
@@ -35,30 +54,34 @@ showBtn.onclick = function () {
 
 //add site li to list ul
 function addSiteToList(site, url) {
+  //remove noneDiv if starting from empty
+  if (document.getElementById("noneDiv")) {
+    document.getElementById("noneDiv").remove()
+  }
   //create link
-  var link = document.createElement('A')
-  link.href = 'https://' + validateURL(url)
+  var link = document.createElement("A")
+  link.href = "https://" + validateURL(url)
   link.target = "_blank" //new tab
   link.title = "Open link"
   var text = document.createTextNode(site)
   link.appendChild(text)
 
   //create remove button for li element
-  var remove = document.createElement('span')
+  var remove = document.createElement("span")
   remove.setAttribute("id", site)
   remove.className = "removable"
   remove.title = "Remove URL from SFW"
-  var removeText = document.createTextNode('×')
+  var removeText = document.createTextNode("×")
   remove.appendChild(removeText)
   //remove function onclick event
-  remove.onclick = function () {
+  remove.onclick = function() {
     chrome.storage.sync.remove(site)
     //remove li from list in DOM
     list.removeChild(document.getElementById("li-" + site))
   }
 
   //create li and add to list ul
-  var node = document.createElement('li')
+  var node = document.createElement("li")
   node.setAttribute("id", "li-" + site)
   node.appendChild(link)
   node.appendChild(remove)
@@ -69,7 +92,7 @@ function showURLs() {
   //empty ul first
   while (list.hasChildNodes()) list.removeChild(list.lastChild)
   //populate ul
-  chrome.storage.sync.get(null, function (result) {
+  chrome.storage.sync.get(null, function(result) {
     var urls = Object.keys(result)
     console.log(result)
     urls.forEach(s => {
@@ -82,5 +105,5 @@ function showURLs() {
 //handle URLs
 function validateURL(url) {
   //strip https:// or http:// from front of entered URL
-  return url.replace(urlRE, '')
+  return url.replace(urlRE, "")
 }
